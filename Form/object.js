bd9eb48fbc8ebcf0ca2f0e3f39df3aba92858767
Diwash <dopkhrel111@gmail.com>
2024-06-16 22:31:42 +0545

    geting data from from to console

---
+++ Form/object.js
@@ -0,0 +1,6 @@
+function handleFormSubmission(e) {
+  let pNumber = document.getElementById("phoneNumber").value;
+  let lName = document.getElementById("lastName").value;
+  let uName = document.getElementById("username").value;
+  console.log(lName, uName, pNumber);
+}
