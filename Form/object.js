c72d05e5cbcc8ca14a84a702ca17b9ffa13e2bd3
Diwash <dopkhrel111@gmail.com>
2024-06-19 06:48:26 +0545

    adding new practice

--- Form/object.js
+++ Form/object.js
@@ -29,37 +29,42 @@
 // delete user.isMarried;
 // console.log(user);
 
-
-document.getElementById("userForm").addEventListener("submit", handleFormSubmission);
+document
+  .getElementById("userForm")
+  .addEventListener("submit", handleFormSubmission);
 
 function handleFormSubmission(e) {
-    e.preventDefault(); // Prevent form submission
-    let pNumber = document.getElementById("phoneNumber").value;
-    let lName = document.getElementById("lastName").value;
-    let uName = document.getElementById("username").value;
-    console.log(lName, uName, pNumber);
+  e.preventDefault(); // Prevent form submission
+  let pNumber = document.getElementById("phoneNumber").value;
+  let lName = document.getElementById("lastName").value;
+  let uName = document.getElementById("username").value;
+  console.log(lName, uName, pNumber);
 }
 
 let user = {
-    personalDetail: {
-        fullName: "ram",
-        lastName: "thapa",
-        adderss: {
-            city: "kathmandu",
-            pincode: "546",
-            country: "Nepal",
-        },
+  personalDetail: {
+    fullName: "ram",
+    lastName: "thapa",
+    adderss: {
+      city: "kathmandu",
+      pincode: "546",
+      country: "Nepal",
     },
-    getFavCOlor: (day) => {
-        return day ? "RED" : "BLACK";
-    },
-    email: "ram@gmail.com",
-    isMarried: false,
-    genger: "MALE",
-    age: 34,
+  },
+  getFavColor: (day) => {
+    return day ? "RED" : "BLACK";
+  },
+  email: "ram@gmail.com",
+  isMarried: false,
+  genger: "MALE",
+  age: 34,
 };
 
 console.log(user.personalDetail.adderss.pincode);
 user.personalDetail.fullName = "Ram Bahadur Adhikari";
 delete user.isMarried;
 console.log(user);
+console.log(user.getFavColor(true));
+
+let keys = Object.keys(user);
+console.log(keys);
